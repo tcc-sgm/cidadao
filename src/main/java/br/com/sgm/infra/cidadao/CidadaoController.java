2631c0801b7cc69ae76f12a4ecdf56477911a34f
andregraciano16 <andregraciano16@gmail.com>
2021-04-04 17:18:54 -0300

    adicionando anotação CrossOrigin

--- src/main/java/br/com/sgm/infra/cidadao/CidadaoController.java
+++ src/main/java/br/com/sgm/infra/cidadao/CidadaoController.java
@@ -1,18 +1,21 @@
 package br.com.sgm.infra.cidadao;
 
-import br.com.sgm.cidadao.Imposto;
-import br.com.sgm.infra.stur.SturClient;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import br.com.sgm.cidadao.Imposto;
+import br.com.sgm.infra.stur.SturClient;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("imposto/")
 public class CidadaoController {
