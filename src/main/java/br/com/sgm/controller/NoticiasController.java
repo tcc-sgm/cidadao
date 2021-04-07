b3e680cf41bce9627a0bda1df69fa93c0d4769a9
andregraciano16 <andregraciano16@gmail.com>
2021-04-06 22:35:10 -0300

    adicionando anotação crossorigin

--- src/main/java/br/com/sgm/controller/NoticiasController.java
+++ src/main/java/br/com/sgm/controller/NoticiasController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/noticias")
 public class NoticiasController {
